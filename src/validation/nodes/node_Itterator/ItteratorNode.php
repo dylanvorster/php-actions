ac01f9baeb63ccbb0f4e3103673cdbf4ffb6590b
Dylan Vorster <dylan@eezipay.com>
2015-10-18 11:18:55 +0200

    Improved itteration options to either match every itteration, or pass at least one

--- src/validation/nodes/node_Itterator/ItteratorNode.php
+++ src/validation/nodes/node_Itterator/ItteratorNode.php
@@ -4,15 +4,28 @@
  */
 class ItteratorNode extends ValidationContainerNode{
 	
+	protected $matchAll;
+	
+	public function __construct() {
+		parent::__construct();
+		$this->matchAll = false;
+	}
+
 	public function match($value) {
+		
 		if(!is_array($value)){
 			throw new ValidationException("Value is not an array");
 		}
 		$exceptions = [];
+		
 		foreach ($value as $v) {
 			try{
 				$this->validateChildren($v);
-				return true;
+				
+				//if we only need one, them simply pass the test
+				if(!$this->matchAll){
+					return true;
+				}
 			}catch(ValidationException $ex){
 				$exceptions[] = $ex;
 			}
@@ -21,5 +34,16 @@ class ItteratorNode extends ValidationContainerNode{
 			throw new ValidationException("There was not at least one child in the itterator node which passed the next node validation test");
 		}
 		return true;
+	}
+	
+	public function deserialize($data) {
+		parent::deserialize($data);
+		$this->matchAll = $data['itteratorType'] === 'ALL';
+	}
+	
+	public function serialize() {
+		$temp = parent::serialize();
+		$temp['itteratorType'] = $this->matchAll?'ALL':'ONE';
+		return $temp;
 	}
 }
